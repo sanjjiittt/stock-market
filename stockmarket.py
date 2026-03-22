2e4d5a495d4f83312e9c6092811571a8a756f065
sanjjiittt <sanjitprasadwork@gmail.com>
2026-03-22 15:19:41 +0530

    Intial commit

---
+++ stockmarket.py
@@ -0,0 +1,40 @@
+import yfinance as yf
+import json
+import matplotlib.pyplot as mlt
+import os
+
+base_path = os.path.dirname(__file__)
+file_path = os.path.join(base_path, "stocks.json")
+
+with open(file_path, "r") as file:
+    tsym=json.load(file)
+while True:
+    try: 
+        stock=input("Enter your Stock name: ").title()
+        timeperiod=input("Enter time period(1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max): ")
+        assert timeperiod in '1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max'
+        ticker=yf.Ticker(tsym.get(stock))
+    except (AttributeError,AssertionError):
+        print("Error! Try again")
+    else:
+        break
+dateIndex=1
+df=ticker.history(period=timeperiod)
+date=[]
+price=[]
+while True:
+    if dateIndex>len(df.index):
+        break
+    else:
+        date.append(str(df.index[dateIndex-1]).split()[0])
+        price.append(df.iloc[dateIndex-1,3])
+        dateIndex+=1
+if df.iloc[len(df.index)-1,0]>df.iloc[0,3]:
+    icolor='green'
+else:
+    icolor='#9c0606'
+mlt.plot(date,price,color=icolor)
+mlt.grid()
+mlt.show()
+
+
