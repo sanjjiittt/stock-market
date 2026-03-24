0c4c4df68270e21100f01a3031c00017c3c214cc
sanjjiittt <sanjitprasadwork@gmail.com>
2026-03-25 00:37:29 +0530

    predictor project

---
+++ stockMarketPredictor.py
@@ -0,0 +1,46 @@
+import yfinance as yf
+import json 
+import numpy as np
+import os
+
+
+base_path = os.path.dirname(__file__)
+file_path = os.path.join(base_path, "stocks.json")
+
+with open(file_path, "r") as file:
+    tickerNames=json.load(file)
+
+
+while True:
+    try:
+        stockName=input("Enter stock name: ").title()
+        assert stockName != None
+        tickerSymbol=yf.Ticker(tickerNames.get(stockName))
+    except (AttributeError,AssertionError):
+        print("Invalid stock name")
+    else:
+        break
+    
+while True:
+    try:
+        timePeriod=input("Time peroid: (5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max): ")
+        assert timePeriod in ['5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max']
+    except AssertionError:
+        print("Invalid time period")
+    else:
+        break
+    
+def getStockdetails(tickerSymbol,timePeriod):
+    df=tickerSymbol.history(timePeriod)
+    date=[]
+    closingPrice=[]
+    for i in range(len(df.index)):
+        date.append(str(df.index[i].date()))
+        closingPrice.append(round(float(df.iloc[i,3]),3))
+
+    return np.array(date),np.array(closingPrice)
+
+date,closingPrice=getStockdetails(tickerSymbol,timePeriod)
+
+print(date)
+print(closingPrice)
