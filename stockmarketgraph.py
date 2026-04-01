e6534280a4267e8de15c93138c81a9da07e37c03
sanjjiittt <sanjitprasadwork@gmail.com>
2026-04-01 23:13:02 +0530

    live updatation with few work remaining

--- stockmarketgraph.py
+++ stockmarketgraph.py
@@ -3,6 +3,8 @@ import json
 import numpy as np
 import os
 import matplotlib.pyplot as mlt
+from matplotlib.animation import FuncAnimation
+import time 
 
 
 base_path = os.path.dirname(__file__)
@@ -11,10 +13,12 @@ file_path = os.path.join(base_path, "stocks.json")
 with open(file_path, "r") as file:
     tickerNames=json.load(file)
 
-stockName=""
 
 def getStock():
     global stockName
+    global tickerSymbol
+    global timePeriod
+
     while True:
         try:
             stockName=input("Enter stock name: ").title()
@@ -34,53 +38,71 @@ def getStock():
         else:
             break
 
-    return {"symbol":tickerSymbol,"time":timePeriod}
+    
     
 
-def getStockHistory(tickerSymbol,timePeriod):
-    if timePeriod in ['1d','5d']:
-        df=tickerSymbol.history(timePeriod,interval='5m')
-        date=[]
-        closingPrice=[]
+def getStockHistory():
+    date=[]
+    closingPrice=[]
+    if timePeriod == '1d':
+        df=tickerSymbol.history(timePeriod,interval='1m')
+        for i in range(len(df.index)):
+            date.append(str(df.index[i].time()))
+            closingPrice.append(round(float(df.iloc[i,3]),3))
+    elif timePeriod == '5d':
+        df=tickerSymbol.history(timePeriod,interval='1m')
         for i in range(len(df.index)):
             date.append(str(df.index[i]))
             closingPrice.append(round(float(df.iloc[i,3]),3))
     elif timePeriod in ['1mo','3mo','6mo']:
         df=tickerSymbol.history(timePeriod,interval='1d')
-        date=[]
-        closingPrice=[]
         for i in range(len(df.index)):
             date.append(str(df.index[i].date()))
             closingPrice.append(round(float(df.iloc[i,3]),3))
     else:
         df=tickerSymbol.history(timePeriod,interval='1d')
-        date=[]
-        closingPrice=[]
         for i in range(len(df.index)):
             date.append(str(df.index[i].date()))
             closingPrice.append(round(float(df.iloc[i,3]),3))
 
+    return date,closingPrice
 
-    return np.array(date),np.array(closingPrice)
 
-def plotStock(history):
-    global stockName
-    date=history[0]
-    price=history[1]
-    if price[0]>price[-1]:
-        pickColor="red"
+
+def latestPrice():
+    if timePeriod == '1d':
+        df=tickerSymbol.history(timePeriod,interval='1m')
+        return str(df.index[-1].time()), round(float(df.iloc[-1,3]),3)
+    elif timePeriod == '5d':
+        df=tickerSymbol.history(timePeriod,interval='1m')
+        return str(df.index[-1]), round(float(df.iloc[-1,3]),3)
+    elif timePeriod in ['1mo','3mo','6mo']:
+        df=tickerSymbol.history(timePeriod,interval='1d')
+        return str(df.index[-1].date()), round(float(df.iloc[-1,3]),3)
     else:
-        pickColor="green"
-    mlt.plot(date,price,color=pickColor)
-    mlt.xticks(date[::len(date)//5])
-    mlt.xlabel("Date")
-    mlt.ylabel("Price in Dollars")
-    mlt.title(stockName + " (rough estimation)")
-    mlt.grid()
-    mlt.show()
+        df=tickerSymbol.history(timePeriod,interval='1d')
+        return str(df.index[-1].date()), round(float(df.iloc[-1,3]),3)
+  
 
+def update(frame):
+    oldDate,oldPrice=history[0],history[1]
+    lDate,lPrice=latestPrice()
+    oldDate.append(lDate)
+    oldPrice.append(lPrice)
+    line.set_data(oldDate,oldPrice)
+    axis.relim()
+    return line,
 
 
 stockDetails=getStock()
-history=getStockHistory(stockDetails["symbol"],stockDetails["time"])
-plotStock(history)
+history=getStockHistory()
+fig,axis=mlt.subplots()
+line,=axis.plot(history[0],history[1])
+mlt.xticks(history[0][::len(history[0])//5])
+mlt.grid()
+mlt.xlabel("Date")
+mlt.ylabel("Price in Dollars")
+mlt.title(stockName + " (rough estimation)")
+ani = FuncAnimation(fig, update, interval=60000, cache_frame_data=False)
+mlt.show()
+
